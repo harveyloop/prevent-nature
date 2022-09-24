dadf0e9be62e01b3669e8660ec800d5f37cda812
Harvey <yerik05vh@gmail.com>
2022-09-24 12:59:04 -0500

    animation and logic heart local storage completed

--- src/actionLike.js
+++ src/actionLike.js
@@ -1,4 +1,5 @@
 const actionLike = () => {
+  let dbLike = [];
   document.addEventListener("click", likeProduct);
   function likeProduct(e) {
     console.log(e.target.tagName);
@@ -9,7 +10,9 @@ const actionLike = () => {
     ) {
       let varOne = e.target.firstElementChild;
       let varTwo = varOne.firstElementChild;
-      e.target.classList.toggle("like-active");
+
+      getProdcutData(e.target);
+
       if (varOne.className.baseVal === "item-love-svg like-active-svg") {
         varOne.classList.toggle("like-active-svg");
         if (varTwo.className.baseVal === "item-love-path like-active-path") {
@@ -19,15 +22,30 @@ const actionLike = () => {
         varTwo.className.baseVal === "item-love-path like-active-path"
       ) {
         varTwo.classList.toggle("like-active-path");
+      } else {
+        e.target.classList.toggle("like-active");
       }
     } else if (
       e.target.className.baseVal === "item-love-svg" ||
       e.target.className.baseVal === "item-love-svg like-active-svg"
     ) {
-      e.target.classList.toggle("like-active-svg");
-
       let varOne = e.target.firstElementChild;
       let varTwo = e.target.parentNode;
+
+      getProdcutData(varTwo);
+
+      if (
+        varOne.className.baseVal == "item-love-path" &&
+        e.target.className.baseVal == "item-love-svg" &&
+        varTwo.className == "action-item-love"
+      ) {
+        e.target.classList.toggle("like-active-svg");
+      } else if (
+        e.target.className.baseVal === "item-love-svg like-active-svg"
+      ) {
+        e.target.classList.toggle("like-active-svg");
+      }
+
       if (varOne.className.baseVal === "item-love-path like-active-path") {
         varOne.classList.toggle("like-active-path");
       }
@@ -38,19 +56,49 @@ const actionLike = () => {
       e.target.className.baseVal === "item-love-path" ||
       e.target.className.baseVal === "item-love-path like-active-path"
     ) {
-      e.target.classList.toggle("like-active-path");
-
       let varOne = e.target.parentNode;
       let varTwo = varOne.parentNode;
+
+      getProdcutData(varTwo);
+
       if (varOne.className.baseVal === "item-love-svg like-active-svg") {
-        varOne.toggle("like-active-svg");
+        varOne.classList.toggle("like-active-svg");
         if (varTwo.className === "action-item-love like-active") {
           varTwo.classList.toggle("like-active");
         }
       } else if (varTwo.className === "action-item-love like-active") {
         varTwo.classList.toggle("like-active");
+      } else {
+        e.target.classList.toggle("like-active-path");
       }
     }
+  }
+  function getProdcutData(father) {
+    let likeIdProduct = father.getAttribute("productId");
+    let allInfo = {
+      id: `${likeIdProduct}`,
+    };
+    if (dbLike.length === 0) {
+      dbLike = [...dbLike, allInfo];
+      console.log("antes", dbLike);
+    } else {
+      let valdate = false;
+      dbLike.forEach((item) => {
+        if (item.id == allInfo.id) {
+          valdate = true;
+        }
+      });
+      dbLike = [...dbLike, allInfo];
+      console.log("antes", dbLike);
+      if (valdate) {
+        let varTemp = dbLike.filter((item) => item.id !== allInfo.id);
+        dbLike = varTemp;
+        valdate = false;
+        console.log("despues", dbLike);
+      }
+    }
+
+    localStorage.setItem("likeProduct", JSON.stringify(dbLike));
   }
 };
 export default actionLike;
