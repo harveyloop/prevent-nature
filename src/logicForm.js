91fbf2875ebd499ec6a94bd2466fdf692d813d6b
Harvey <yerik05vh@gmail.com>
2022-10-10 15:01:14 -0500

    add eye close and open

--- src/logicForm.js
+++ src/logicForm.js
@@ -5,10 +5,14 @@ const logicForm = (input) => {
       if (!validate) {
         input.removeAttribute("type");
         input.setAttribute("type", "text");
+        e.target.removeAttribute("src");
+        e.target.setAttribute("src", "../assets/no-view-password.svg");
         validate = true;
       } else {
         input.removeAttribute("type");
         input.setAttribute("type", "password");
+        e.target.removeAttribute("src");
+        e.target.setAttribute("src", "../assets/view-password.svg");
         validate = false;
       }
     }
